ccd91be88505392fb3b35b113270133f480f5570
Abhishek Mishra <amishr20@uic.edu>
2017-02-08 02:52:59 -0600

    Update and rename Assgn2.sql to Introductory_Assignment.sql

--- Introductory_Assignment.sql
+++ Introductory_Assignment.sql
@@ -1,16 +1,12 @@
 
---1.
- -- The following query fetches the vendor name that sells 'Walnut' at a price less than 14 units.
-
---2. 
-
 --3. 
-	select distinct Vendor_name
-	 from (select RM.Material_ID, S.Vendor_ID from Raw_Materials_t as RM, Supplies_t as S  
-							where RM.Material_ID=S.Material_ID and Material_description = 'Walnut' and S.Unit_price<14) as T1, Vendor_t as T2
-							where T1.Vendor_ID=T2.Vendor_ID 
+select distinct Vendor_name
+from (select RM.Material_ID, S.Vendor_ID from Raw_Materials_t as RM, Supplies_t as S  
+where RM.Material_ID=S.Material_ID and Material_description = 'Walnut' and S.Unit_price<14) as T1, Vendor_t as T2
+where T1.Vendor_ID=T2.Vendor_ID 
+
 --4.
- select Order_ID
+select Order_ID
 from (select Order_ID, O1.Product_ID, Product_Name 
   from Order_line_t O1, Product_t P1
   where O1.Product_ID=P1.Product_ID and Product_Name in ('End Table','Coffee Table')) as Temp
@@ -33,7 +29,7 @@ from Supplies_t as Temp) as S, Raw_Materials_t as R, Vendor_t as V
 where S.Material_ID=R.Material_ID and S.Vendor_ID=V.Vendor_ID and S.Ranking in (1,2)
 order by R.Material_ID, S.Unit_price
 
- --Compose an SQL statement to generate a list of two least expensive vendors (suppliers) for each raw material
+ -- Get 2 least expensive vendors for each raw material !!
 
 select S1.Material_ID
 from Supplies_t as S1
